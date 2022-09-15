e9c76d46fb780838903ce27ef1f76829a3888065
saabrin <90770758+saabrin@users.noreply.github.com>
2022-09-15 13:06:26 +0200

    Initial Commit

---
+++ tic tac toe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tic tac toe
+//
+//  Created by sabrin hakimi on 2022-09-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tic tac toeTests/tic_tac_toeTests.swift
@@ -0,0 +1,36 @@
+//
+//  tic_tac_toeTests.swift
+//  tic tac toeTests
+//
+//  Created by sabrin hakimi on 2022-09-15.
+//
+
+import XCTest
+@testable import tic_tac_toe
+
+final class tic_tac_toeTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
